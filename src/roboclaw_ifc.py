800d59bf4956e3766cac67ebaf5f613e9884c82f
Ken McCormack <ken.mccormack@comcast.net>
2018-10-05 02:37:52 -0700

    control timeout protection

--- src/roboclaw_ifc.py
+++ src/roboclaw_ifc.py
@@ -5,12 +5,25 @@ import roboclaw_driver.roboclaw_driver as rc
 from std_msgs.msg import String
 
 MTR_ADDRESS = 128 
-MAX_DUTY_CYCLE = .25 
+MAX_DUTY_CYCLE = .55 
+
+def timer_callback(event):
+    global last_time
+    if(rospy.get_rostime() - last_time ).to_sec() > 1:
+       rc.DutyM1M2(MTR_ADDRESS, 0, 0)
+       rospy.loginfo("timeout on control")
+    
+    
+
+
 
 
 def twist_callback(msg):
 
     global pub
+    global last_time 
+    last_time = rospy.get_rostime()
+
     #rospy.loginfo("Linear Components: [%f, %f, %f]"%(msg.linear.x, msg.linear.y, msg.linear.z))
     #rospy.loginfo("Angular Components: [%f, %f, %f]"%(msg.angular.x, msg.angular.y, msg.angular.z))
 
@@ -35,6 +48,8 @@ def twist_callback(msg):
 
 def move_robot():
     global pub 
+    global last_time
+    
     rc.Open('/dev/ttyUSB0',115200)
     print rc.ReadMinMaxMainVoltages(MTR_ADDRESS)
 
@@ -42,6 +57,10 @@ def move_robot():
   
 
     rospy.init_node('roboclaw_ifc')
+
+    last_time = rospy.get_rostime()
+    rospy.Timer(rospy.Duration(1), timer_callback)
+   
     rospy.Subscriber("/cmd_vel", Twist, twist_callback, queue_size=1)
     rospy.spin()
 
