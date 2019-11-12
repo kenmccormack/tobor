3d677fa2a4580bab0cb789455e89780bf843959f
ken <ken.mccormack@comacast.net>
2019-11-11 20:15:39 -0800

    transferring to melodic

--- src/teleop.cpp
+++ src/teleop.cpp
@@ -3,10 +3,10 @@
 #include <sensor_msgs/Joy.h>
 
 
-class TeleopTurtle
+class TeleopTurtle 
 {
 public:
-  TeleopTurtle();
+  TeleopTurtle(ros::NodeHandle nh);
 
 private:
   void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
@@ -21,21 +21,24 @@ private:
 };
 
 
-TeleopTurtle::TeleopTurtle():
+TeleopTurtle::TeleopTurtle(ros::NodeHandle nh):
+  nh_(nh),
   linear_(1),
   angular_(0)
 {
 
   nh_.param("axis_linear", linear_, linear_);
   nh_.param("axis_angular", angular_, angular_);
-  nh_.param("scale_angular", a_scale_, 4.20);
-  nh_.param("scale_linear", l_scale_, 5.0);
+  nh_.param("scale_angular", a_scale_, 2.60);
+  nh_.param("scale_linear", l_scale_, 2.5);
+  ROS_INFO("Ascale = %f",a_scale_);
+  ROS_INFO("Lscale = %f",l_scale_);
 
 
   vel_pub_ = nh_.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
 
 
-  joy_sub_ = nh_.subscribe<sensor_msgs::Joy>("joy", 10, &TeleopTurtle::joyCallback, this);
+  joy_sub_ = nh_.subscribe<sensor_msgs::Joy>("/joy", 10, &TeleopTurtle::joyCallback, this);
 
 }
 
@@ -51,7 +54,8 @@ void TeleopTurtle::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "teleop_turtle");
-  TeleopTurtle teleop_turtle;
+  ros::NodeHandle nh;
+  TeleopTurtle teleop_turtle(nh);
 
   ros::spin();
 }
