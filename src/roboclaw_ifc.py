0969865eb322de981801577eb205d110318b00d3
ken <ken.mccormack@comcast.net>
2020-10-25 16:31:34 -0700

    add velocity filter and range check

--- src/roboclaw_ifc.py
+++ src/roboclaw_ifc.py
@@ -5,8 +5,8 @@ import roboclaw_driver.roboclaw_driver as rc
 from std_msgs.msg import String
 
 MTR_ADDRESS = 128 
-MAX_DUTY_CYCLE = .75 
-
+MAX_DUTY_CYCLE = .15 
+MIX_ROTATION = .5
 def timer_callback(event):
     global last_time
     if(rospy.get_rostime() - last_time ).to_sec() > 1:
@@ -15,7 +15,12 @@ def timer_callback(event):
     
     
 
-
+def limit(value, limit):
+    if value > limit:
+	value = limit
+    if value < -1 * limit:
+	value = -1* limit
+    return value
 
 
 def twist_callback(msg):
@@ -27,12 +32,14 @@ def twist_callback(msg):
     #rospy.loginfo("Linear Components: [%f, %f, %f]"%(msg.linear.x, msg.linear.y, msg.linear.z))
     #rospy.loginfo("Angular Components: [%f, %f, %f]"%(msg.angular.x, msg.angular.y, msg.angular.z))
 
-    TR = 3.0
+    TR = MIX_ROTATION 
     v_l = msg.linear.x - TR * msg.angular.z
     v_r = msg.linear.x + TR * msg.angular.z
 
     dutyleft = int( v_l * MAX_DUTY_CYCLE * 32768)
     dutyright= int( v_r * MAX_DUTY_CYCLE * 32768)
+    dutyleft = limit(dutyleft, 32767)
+    dutyright = limit(dutyright, 32767)
     rc.DutyM1M2(MTR_ADDRESS, dutyleft, dutyright)
 
     rospy.loginfo("[%d , %d]"%(dutyleft, dutyright))
