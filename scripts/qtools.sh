62185a2dc63a2a143776d64c21ecce4b936b0421
ken.mccormack <ken.mccormack@comcast.net>
2020-11-14 14:07:39 -0800

    Startup script fixed errors

--- scripts/qtools.sh
+++ scripts/qtools.sh
@@ -5,4 +5,6 @@ cd $BASE_DIR
 catkin_make install
 tar -czf /tmp/quantum_install.tar.gz install
 scp      /tmp/quantum_install.tar.gz user@quantum:/tmp
+ssh user@quantum 'rm -rf ~/quantum_ws'
+ssh user@quantum 'mkdir ~/quantum_ws'
 ssh user@quantum 'tar xf /tmp/quantum_install.tar.gz -C ~/quantum_ws'
